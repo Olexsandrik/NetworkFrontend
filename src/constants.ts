a94b2d8db58a8a5c306bcf8c90438ba864d807a4
Olexsandrik <119703838+Olexsandrik@users.noreply.github.com>
2024-12-21 14:47:22 +0200

    production deploy

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,14 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:3000"
-    : "http://localhost:3000"
+    ? "https://networkbackend-1j8f.onrender.com"
+    : "https://networkbackend-1j8f.onrender.com"
+
+
+
+
+
+
+
+
+    // ? "http://localhost:3000"
+    // : "http://localhost:3000" production
